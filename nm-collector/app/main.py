59461bc730a34d6bd5727a800c3d301590c93352
DvaMishkiLapa <artem132rus@gmail.com>
2022-05-02 13:56:32 +0300

    Add scan request logic by args

--- nm-collector/app/main.py
+++ nm-collector/app/main.py
@@ -1,6 +1,7 @@
-import os
 import shlex
 import subprocess
+from dataclasses import dataclass
+from datetime import datetime
 
 from sanic import Sanic
 from sanic.config import Config
@@ -8,6 +9,7 @@ from sanic.config import Config
 # from sanic.exceptions import NotFound
 # from sanic.log import logger
 from sanic.response import json
+from sanic_ext import validate
 from sanic_motor import BaseModel
 
 from nmap_xml_parser import NmapXmlInfo
@@ -35,8 +37,6 @@ app.config.update(settings)
 BaseModel.init_app(app)
 
 xml_folder = 'xmls'
-nmap_cmd = f'nmap -sV -O -p- localhost -oX {xml_folder}/scan.xml'
-nmap_args = shlex.split(nmap_cmd)
 
 
 class Collector(BaseModel):
@@ -49,15 +49,35 @@ def serialz_id(objs: list) -> list:
     return objs
 
 
-@app.get("/scan")
-async def scan(request):
-    result = subprocess.call(nmap_args)
-    scan_result = NmapXmlInfo(f'{xml_folder}/scan.xml', True).info
-    update_result = await Collector.insert_many(scan_result)
+def nmap_scan(host: str, save_xml: bool = False) -> str:
+    nmap_cmd_template = f'nmap {host} -T4 -sV -O -p- -min-parallelism 100 --min-rate 64 -oX'
+    if save_xml:
+        now_datatime = datetime.now().strftime('%m-%d-%Y_%H-%M-%S')
+        xml_path = f'{xml_folder}/{now_datatime}_{host}.xml'
+        nmap_cmd = f'{nmap_cmd_template} {xml_path}'
+        subprocess.call(shlex.split(nmap_cmd))
+        return xml_path
+    nmap_cmd = f'{nmap_cmd_template} -'
+    result = subprocess.Popen(shlex.split(nmap_cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    return result[0].decode('utf-8')
+
+
+@dataclass
+class NmapParams:
+    host: str
+    save_xml: bool
+
+
+@app.post("/scan")
+@validate(json=NmapParams)
+async def scan(request, body: NmapParams):
+    args = request.json
+    scan_result = nmap_scan(args['host'], args['save_xml'])
+    scan_parse = NmapXmlInfo(scan_result, args['save_xml']).info
+    update_result = await Collector.insert_many(scan_parse)
     return json({
-        'data': serialz_id(scan_result),
+        'data': serialz_id(scan_parse),
         'objectIds': [str(x) for x in update_result.inserted_ids],
-        'subprocess_status': result
     }, 200)
 
 app.run(host="0.0.0.0", port=8000, debug=1)
